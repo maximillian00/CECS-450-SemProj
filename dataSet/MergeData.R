0f9668b03bcfdf753acdbbc609721f5fdf88d6e5
JuanRivera-7 <juan_rivera766@hotmail.com>
2023-10-31 11:34:08 -0700

    Update Prime Video title

--- dataSet/MergeData.R
+++ dataSet/MergeData.R
@@ -1,7 +1,11 @@
 
 dt <- read.csv("~/GitHub/CECS-450-SemProj/dataSet/movies_and_tv - Sheet1.csv")
 
-dt$group <- (mean(RottenTomatoes))
+#dt$group <- (mean(RottenTomatoes))
 
-boxplot(values ~ group,dt)
+boxplot(mean(RottenTomatoes) ~ Year, data = dt,
+        main = "Average score for Netflix",
+        xlab = "Netflix",
+        ylab = "Average Rotten Tomatoe score" 
+        )
 
