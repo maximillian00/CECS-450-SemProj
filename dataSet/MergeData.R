841739cff67fd140977593ae16b924b6df339b66
JuanRivera-7 <juan_rivera766@hotmail.com>
2023-10-31 12:08:36 -0700

    Update MergeData.R
    Trying to get box plot working. Found the problem but not the solution. Problem is the Rotten Tomato score column is chr instead of a int/float. Have to convert chr to int/float. I tried as.numeric however since input is "30/100"  fractions, r does not convert to a number.

--- dataSet/MergeData.R
+++ dataSet/MergeData.R
@@ -2,10 +2,15 @@
 dt <- read.csv("~/GitHub/CECS-450-SemProj/dataSet/movies_and_tv - Sheet1.csv")
 
 #dt$group <- (mean(RottenTomatoes))
+temp <- as.integer(c("100/100", "100/100"))
 
-boxplot(mean(RottenTomatoes) ~ Year, data = dt,
+score <- as.numeric(dt$RottenTomatoes)
+
+avg <- lapply(score, mean, na.rm = TRUE)
+
+
+boxplot(avg ~ Netflix, data = dt,
         main = "Average score for Netflix",
         xlab = "Netflix",
         ylab = "Average Rotten Tomatoe score" 
         )
-
