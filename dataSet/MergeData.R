ecaa0ce455475de0159abbef5326b8414eb32abe
JuanRivera-7 <juan_rivera766@hotmail.com>
2023-11-07 11:47:01 -0800

    Update MergeData.R
    Trying to convert list of char to list of decimal

--- dataSet/MergeData.R
+++ dataSet/MergeData.R
@@ -2,9 +2,15 @@
 dt <- read.csv("~/GitHub/CECS-450-SemProj/dataSet/movies_and_tv - Sheet1.csv")
 
 #dt$group <- (mean(RottenTomatoes))
-temp <- as.integer(c("100/100", "100/100"))
 
-score <- as.numeric(dt$RottenTomatoes)
+
+temp <- c("80/100", "100/100")
+decimal_list <- lapply(temp, function(x) eval(parse(text = x)))
+decimal_list <- lapply(decimal_list, function(x) as.numeric(x))
+
+print(decimal_list)
+
+score <- dt$RottenTomatoes
 
 avg <- lapply(score, mean, na.rm = TRUE)
 
@@ -14,3 +20,4 @@ boxplot(avg ~ Netflix, data = dt,
         xlab = "Netflix",
         ylab = "Average Rotten Tomatoe score" 
         )
+
