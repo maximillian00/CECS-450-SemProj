7d052350f007835b403b8fae7c211425fdc7e6ca
JuanRivera-7 <juan_rivera766@hotmail.com>
2023-11-09 11:45:00 -0800

    Update MergeData.R

--- dataSet/MergeData.R
+++ dataSet/MergeData.R
@@ -4,15 +4,13 @@ dt <- read.csv("~/GitHub/CECS-450-SemProj/dataSet/movies_and_tv - Sheet1.csv")
 #dt$group <- (mean(RottenTomatoes))
 
 
-temp <- c("80/100", "100/100")
-decimal_list <- lapply(temp, function(x) eval(parse(text = x)))
-decimal_list <- lapply(decimal_list, function(x) as.numeric(x))
-
-print(decimal_list)
+temp <- c(80/100, 100/100)
+print(temp)
 
 score <- dt$RottenTomatoes
 decimal_list <- lapply(score, function(x) eval(parse(text = x)))
 decimal_list <- lapply(decimal_list, function(x) as.numeric(x))
+decimal_list <- unlist(decimal_list)
 print(decimal_list)
 
 avg <- mean(unlist(decimal_list))
@@ -20,12 +18,12 @@ print(avg)
 
 net <- unique(dt$Netflix)
 
-data <- data.frame(Average = avg, Netflix = net)
+data <- data.frame(Average = decimal_list, Netflix = net)
 
 
-boxplot(data,
+boxplot(decimal_list ~ dt$Netflix,
         main = "Average score for Netflix",
         xlab = "Netflix",
-        ylab = "Average Rotten Tomatoe score" 
+        ylab = "Average score" 
         )
 
