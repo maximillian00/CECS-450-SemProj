14913e3d0f4e8c765fb9a4a759b9c02456726295
JuanRivera-7 <62785733+JuanRivera-7@users.noreply.github.com>
2023-11-30 22:25:47 -0800

    Delete Files directory

--- Files/MergeData.R
+++
@@ -1,6 +0,0 @@
-
-MoviesOnStreamingPlatforms <- read.csv("~/GitHub/CECS-450-SemProj/dataSet/MoviesOnStreamingPlatforms.csv")
-
-tv_shows <- read.csv("~/GitHub/CECS-450-SemProj/dataSet/tv_shows.csv")
-
-dt <- merge(MoviesOnStreamingPlatforms, tv_shows)
