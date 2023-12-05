7036d424e0a9cc296ca1b389d218f0b6891145c4
iochuu <83665357+iochuu@users.noreply.github.com>
2023-12-04 21:38:30 -0800

    Barplots and Images
    Average Price per Score (w ads, wo ads)

--- dataSet/MergeData.R
+++ dataSet/MergeData.R
@@ -1,47 +1,43 @@
+#Create Barplots to compare Average Rotten Tomato Score over Price 
+#per Streaming Service (IE the Value per Streaming Service)
 
-dt <- read.csv("~/GitHub/CECS-450-SemProj/dataSet/movies_and_tv - Sheet1.csv")
+#Average Rotten Tomato Score per Streaming Service
+prime_avg <- mean(unlist(prime$RottenTomatoes))
+net_avg <- mean(unlist(net$RottenTomatoes))
+hulu_avg <- mean(unlist(hulu$RottenTomatoes))
+dis_avg <- mean(unlist(dis$RottenTomatoes))
 
-#dt$group <- (mean(RottenTomatoes))
-<<<<<<< HEAD
-#comment
-=======
->>>>>>> acbf5d96d59eb119a81d4d0dd14bb3c3adc0dc74
+prime_avg <- as.numeric(prime_avg)
+net_avg <- as.numeric(net_avg)
+hulu_avg <- as.numeric(hulu_avg)
+dis_avg <- as.numeric(dis_avg)
 
-# random testing
-temp <- c(80/100, 100/100)
-print(temp)
-####################
+# Calculate the average score per each streaming service's price with ads
+prime_value<- (prime_avg/prime_price) 
+net_value_ads<-(net_avg/netflix_ads) 
+hulu_value_ads<- (hulu_avg/hulu_ads) 
+disney_value_ads<- (dis_avg/disney_ads) 
 
-score <- dt$RottenTomatoes #gets column of Rotten Tomatoe from dataset
+# Do the same for prices without ads 
+net_value<- (net_avg/netflix_no_ads) 
+hulu_value<- (hulu_avg/hulu_no_ads) 
+disney_value<- (dis_avg/disney_no_ads) 
 
-#converts char column to double
-decimal_list <- lapply(score, function(x) eval(parse(text = x))) 
-decimal_list <- lapply(decimal_list, function(x) as.numeric(x))
-###################################
+# Create the bar plot for average price per score with ads
+value_ads <-c(prime_value, net_value_ads, hulu_value_ads, disney_value_ads)
+barplot(value_ads, names.arg = labels, 
+        col = c("purple", "red", "green", "blue"), 
+        main = "Value per Streaming Service with Ads", 
+        ylab = "Price per Average Score", 
+        xlab = "Streaming Service",
+        ylim = c(0,0.08))
 
-
-decimal_list <- unlist(decimal_list) #converts column to list (2d array -> array)
-print(decimal_list)
-
-# More random testing
-avg <- mean(unlist(decimal_list))
-print(avg)
-###############################
-
-
-# Plots Netflix average Rotten Tomato score for show and movies
-# 0 is another streaming service and 1 is Netflix's score
-boxplot(decimal_list ~ dt$Netflix,
-        main = "Average score for Netflix V.S. Other Streaming Platforms",
-        xlab = "Netflix",
-        ylab = "Average score" 
-        )
-
-#Plots average score shows and movies on Netflix
-# 0 is movies and 1 is shows
-boxplot(decimal_list ~ dt$Type,
-        main = "Show V.S. Movies Average Score on Netflix",
-        xlab = "Netflix",
-        ylab = "Average score" 
-        )
+# Create the bar plot for average price per score without ads
+value_no_ads <-c(prime_value, net_value, hulu_value, disney_value)
+barplot(value_no_ads, names.arg = labels,  
+        col = c("purple", "red", "green", "blue"), 
+        main = "Value per Streaming Service without Ads", 
+        ylab = "Average Score over Price", 
+        xlab = "Streaming Service",
+        ylim = c(0,0.08))
 
